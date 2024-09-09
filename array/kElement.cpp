680b7066c63e6ea91c586af4e2ef98aaab736136
tasmia monzoor <tasmiamonzoor@gmail.com>
2024-09-09 14:13:03 +0600

    new files

--- array/kElement.cpp
+++ array/kElement.cpp
@@ -34,25 +34,40 @@ using namespace std;
 //using bucket sort and size of bucket is equal to the size of input array
 vector<int> topKFrequent(vector<int>& nums, int k) {
     unordered_map<int, int> umap;
-    for(int x: nums){
+    for (size_t i = 0; i < nums.size(); ++i) {
+        int x = nums[i];
         umap[x]++;
     }
+    /*   for(int x: nums){
+        umap[x]++;
+    }*/
+
     vector<vector<int>> bucket(nums.size() + 1);
-    for(auto x: umap){
-        bucket[x.second].push_back(x.first);
+    for (auto it = umap.begin(); it != umap.end(); ++it) {
+        int frequency = it->second;
+        int num = it->first;
+        bucket[frequency].push_back(num);
     }
+    
+    /* for(auto it: umap){
+        bucket[it.second].push_back(it.first);
+    }*/
+
+
     vector<int> res;
-    for(int i = bucket.size() - 1; i >= 0; i--){
-        for(int j = 0; j < bucket[i].size(); j++){
+    for (int i = bucket.size() - 1; i >= 0; --i) {
+        for (size_t j = 0; j < bucket[i].size(); ++j) {
             res.push_back(bucket[i][j]);
-            if(res.size() == k){
+            if (res.size() == k) {
                 return res;
             }
         }
     }
-    return res;
-    //time complexity of above approach is O(n) and space complexity is O(n)
+
+
+    return res; // Add this line to ensure the function returns a value in all cases
 }
+
 
 int main(){
     vector<int> nums = {1, 1, 1, 2, 2, 3};
